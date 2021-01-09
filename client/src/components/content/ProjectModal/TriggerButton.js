2cdaf937b679d3617d44c79ce57a4d0cbf4fcab6
afif ahmed <afifahmed456123@gmail.com>
2021-01-09 17:54:33 +0530

    fab button

--- client/src/components/content/ProjectModal/TriggerButton.js
+++ client/src/components/content/ProjectModal/TriggerButton.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { Tooltip, Fab } from "@material-ui/core";
-import AddIcon from "@material-ui/icons/Add";
+import React from 'react';
+import { Tooltip, Fab } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
 
 const style = {
   margin: 0,
-  top: "auto",
+  top: 'auto',
   right: 20,
-  bottom: 20,
-  left: "auto",
-  position: "fixed",
-  backgroundColor: "#43dde6",
+  bottom: 30,
+  left: 'auto',
+  position: 'fixed',
+  backgroundColor: 'orange',
 };
 
 const Trigger = ({ triggerText, buttonRef, showModal }) => {
   return (
-    <Tooltip title="Create Project" aria-label="add">
-      <Fab style={style} color="primary" ref={buttonRef} onClick={showModal}>
+    <Tooltip title='Create Project' aria-label='add'>
+      <Fab style={style} color='primary' ref={buttonRef} onClick={showModal}>
         <AddIcon />
       </Fab>
     </Tooltip>
