8a40325ac56526d3d3c8e65727d6902d930047cb
afif ahmed <afifahmed456123@gmail.com>
2021-03-27 23:12:18 +0530

    build folder

--- server.js
+++ server.js
@@ -55,13 +55,13 @@ app.use('/api/user', user);
 const port = process.env.PORT || 5000;
 
 //sercve static assets if in production
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-}
+// }
 app.listen(port, () =>
   console.log(`Server is up and running on port ${port}!`)
 );
